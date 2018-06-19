245ac4483f53526953eeddc7d3ce2d1ca0022a63
Gabriel Tonatiuh Ramos Vilchis <Tona-@Macbook-Pro-2.local>
2018-06-19 15:19:53 -0700

    added updated proxy server file.

--- README.md
+++
@@ -1,2 +0,0 @@
-# proxy_TonaR
-Proxy server to render all components
--- server/index.jsx
+++ server/index.jsx
@@ -5,77 +5,81 @@ const url = require('url')
 
 const app = express();
 
-app.use('/listingDescription/:id', (request, response) => {
-  const urlAddress = new url.URL( request.params.id , 'http://127.0.0.1:3001/');
-  response.redirect(307, urlAddress);
-});
-
-app.use('/reviews', (request, response) => {
-  const urlAddress = new url.URL( request.originalUrl , 'http://127.0.0.1:3003/');
-  response.redirect(307, urlAddress);
-});
-
-app.use('/theReviews', (request, response) => {
-  const urlAddress = new url.URL( request.url , 'http://127.0.0.1:3003/');
-  response.redirect(307, urlAddress);
-});
-
-app.use('/booking', (request, response) => {
-  const urlAddress = new url.URL( request.originalUrl , 'http://127.0.0.1:3002/');
-  response.redirect(307, urlAddress);
-});
-
-app.use('/theBooking/:id', (request, response) => {
-  const urlAddress = new url.URL( request.params.id , 'http://127.0.0.1:3002/');
-  response.redirect(307, urlAddress);
-});
-
-app.use('/filterListings', (request, response) => {
-  const urlAddress = new url.URL( request.originalUrl , 'http://127.0.0.1:3004/');
-  response.redirect(307, urlAddress );
-});
-
-app.use('/theFilterListing/:id', (request, response) => {
-  const urlAddress = new url.URL( request.params.id , 'http://127.0.0.1:3004/');
-  response.redirect(307, urlAddress);
-});
-
-app.use('/theTitleGallery/:id', (request, response) => {
-  const urlAddress = new url.URL( request.params.id , 'http://127.0.0.1:3005/');
-  console.log(request)
-  response.redirect(307, urlAddress);
-});
-
-app.use('/theTitleGallery', (request, response) => {
-  const urlAddress = new url.URL( request.originalUrl , 'http://127.0.0.1:3005/');
-  response.redirect(307, urlAddress );
-});
-
-app.use('/headerphotos/:id', (request, response) => {
-  const urlAddress = new url.URL( request.params.id , 'http://127.0.0.1:3005/headerphotos/');
-  response.redirect(307, urlAddress);
-});
+app.use(bodyParser.json());
 
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
   if (req.method === 'OPTIONS') {
     res.header(
       'Access-Control-Allow-Mehods',
-      'GET, POST, PUT, PATCH, DELETE',
+      'GET, POST, PUT, PATCH, DELETE'
     );
     return res.status(200).json({});
   }
   next();
 });
 
-app.use(express.static(path.join(__dirname, '../server')));
 app.get('/favicon.ico', (req, res) => res.status(204));
 
+app.use(express.static(path.join(__dirname, '../server')));
 
-const port = 3000;
+app.use('/listingDescription/:id', (request, response) => {
+  const urlAddress = new url.URL( request.params.id , 'http://nappbnb-env-1.yykbu3dn27.us-east-1.elasticbeanstalk.com/');
+  response.redirect(307, urlAddress);
+});
+
+app.use('/rooms/', (request, response) => {
+  const urlAddress = new url.URL( request.originalUrl , 'http://nappbnb-env-1.yykbu3dn27.us-east-1.elasticbeanstalk.com/');
+  response.redirect(307, urlAddress);
+});
+
+app.use('/reviews', (request, response) => {
+  const urlAddress = new url.URL( request.originalUrl , 'http://nappbnbreviews.us-west-1.elasticbeanstalk.com/');
+  response.redirect(307, urlAddress);
+});
+
+app.use('/theReviews', (request, response) => {
+  const urlAddress = new url.URL( request.url , 'http://nappbnbreviews.us-west-1.elasticbeanstalk.com/');
+  response.redirect(307, urlAddress);
+});
+
+app.use('/booking', (request, response) => {
+  const urlAddress = new url.URL( request.originalUrl , 'http://booking-service-logan.us-west-1.elasticbeanstalk.com/');
+  response.redirect(307, urlAddress);
+});
+
+app.use('/theBooking/:id', (request, response) => {
+  const urlAddress = new url.URL( request.params.id , 'http://booking-service-logan.us-west-1.elasticbeanstalk.com/');
+  response.redirect(307, urlAddress);
+});
+
+app.use('/filterListings', (request, response) => {
+  const urlAddress = new url.URL( request.originalUrl , 'http://nappfilterlistingryan-env.n93dfz3d6f.us-west-1.elasticbeanstalk.com/');
+  response.redirect(307, urlAddress );
+});
+
+app.use('/theFilterListing/:id', (request, response) => {
+  const urlAddress = new url.URL( request.params.id , 'http://nappfilterlistingryan-env.n93dfz3d6f.us-west-1.elasticbeanstalk.com/');
+  response.redirect(307, urlAddress);
+});
+
+
+app.use('/theTitleGallery/:id', (request, response) => {
+  const urlAddress = new url.URL( request.params.id , 'http://airbnb-title-galley.us-west-1.elasticbeanstalk.com/');
+  response.redirect(307, urlAddress );
+});
+
+app.use('/headerphotos/', (request, response) => {
+  const urlAddress = new url.URL( request.originalUrl , 'http://airbnb-title-galley.us-west-1.elasticbeanstalk.com/');
+  response.redirect(307, urlAddress);
+});
+
+
+
+const port = 8081;
 app.listen(port, () => console.log(`listening on port ${port}`));
